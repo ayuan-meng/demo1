74937450c242e9352bb2d7c2c990a5bcac3db84a
yuanmeng <itcats_cn@itcats.cn>
2022-01-20 17:50:58 +0800

    Initial commit初始化

---
+++ src/main/java/com/example/demo/HelloWrold.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+/**
+ * @Author: Meng.Yuan
+ * @Description: TODO
+ * @DateTime: 2022/1/20 17:34
+ **/
+public class HelloWrold {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
