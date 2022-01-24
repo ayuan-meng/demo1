76d831ba4dd96b2c17783ff18254ccdb707ce773
yuanmeng <itcats_cn@itcats.cn>
2022-01-24 11:47:21 +0800

    添加注释

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     HelloWorld
   }
 }
